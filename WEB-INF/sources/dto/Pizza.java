a38e6c70184c99f0932bc102b1dcd0b92d441c56
Samy Van Calster <samy.vancalster.etu@univ-lille.fr>
2024-02-21 14:16:16 +0100

    Ajout de la java-doc pour la classe Pizza

--- WEB-INF/sources/dto/Pizza.java
+++ WEB-INF/sources/dto/Pizza.java
@@ -2,6 +2,9 @@ package dto;
 
 import java.util.List;
 
+/**
+ * Représente une pizza.
+ */
 public class Pizza {
     
     private final String NAME;
@@ -9,6 +12,14 @@ public class Pizza {
     private double prixBase;
     private List<Ingredient> ingredients;
 
+    /**
+     * Constructeur de la classe Pizza.
+     *
+     * @param nAME       Le nom de la pizza.
+     * @param pate       Le type de pâte de la pizza.
+     * @param prixBase   Le prix de base de la pizza.
+     * @param ingredients La liste des ingrédients de la pizza.
+     */
     public Pizza(String nAME, String pate, double prixBase, List<Ingredient> ingredients) {
         NAME = nAME;
         this.pate = pate;
@@ -16,30 +27,65 @@ public class Pizza {
         this.ingredients = ingredients;
     }
 
+    /**
+     * Retourne le nom de la pizza.
+     *
+     * @return Le nom de la pizza.
+     */
     public String getNAME() {
         return NAME;
     }
 
+    /**
+     * Retourne le type de pâte de la pizza.
+     *
+     * @return Le type de pâte de la pizza.
+     */
     public String getPate() {
         return pate;
     }
 
+    /**
+     * Retourne le prix de base de la pizza.
+     *
+     * @return Le prix de base de la pizza.
+     */
     public double getPrixBase() {
         return prixBase;
     }
 
+    /**
+     * Retourne la liste des ingrédients de la pizza.
+     *
+     * @return La liste des ingrédients de la pizza.
+     */
     public List<Ingredient> getIngredients() {
         return ingredients;
     }
 
+    /**
+     * Modifie le type de pâte de la pizza.
+     *
+     * @param pate Le nouveau type de pâte de la pizza.
+     */
     public void setPate(String pate) {
         this.pate = pate;
     }
 
+    /**
+     * Modifie le prix de base de la pizza.
+     *
+     * @param prixBase Le nouveau prix de base de la pizza.
+     */
     public void setPrixBase(double prixBase) {
         this.prixBase = prixBase;
     }
 
+    /**
+     * Modifie la liste des ingrédients de la pizza.
+     *
+     * @param ingredients La nouvelle liste des ingrédients de la pizza.
+     */
     public void setIngredients(List<Ingredient> ingredients) {
         this.ingredients = ingredients;
     }    
