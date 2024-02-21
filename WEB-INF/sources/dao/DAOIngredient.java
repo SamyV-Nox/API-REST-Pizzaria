c10bff869d8ef6bd0b6d1b5a439d8007aad395f8
Samy Van Calster <samy.vancalster.etu@univ-lille.fr>
2024-02-21 15:03:33 +0100

    Ajout de la java-doc pour les interfaces DAOIngredient et DAOPizza

--- WEB-INF/sources/dao/DAOIngredient.java
+++ WEB-INF/sources/dao/DAOIngredient.java
@@ -4,12 +4,32 @@ import java.util.List;
 
 import dto.Ingredient;
 
+/**
+ * Interface pour le DAO (Data Access Object) des ingrédients.
+ * Définit les opérations de base pour accéder aux données des ingrédients.
+ */
 public interface DAOIngredient {
 
+    /**
+     * Récupère tous les ingrédients.
+     *
+     * @return Une liste contenant tous les ingrédients.
+     */
     public List<Ingredient> findAll();
 
+    /**
+     * Récupère un ingrédient par son identifiant.
+     *
+     * @param id L'identifiant de l'ingrédient.
+     * @return L'ingrédient correspondant à l'identifiant, ou null s'il n'est pas trouvé.
+     */
     public Ingredient findById(int id);
 
+    /**
+     * Enregistre un nouvel ingrédient.
+     *
+     * @param ingredient L'ingrédient à enregistrer.
+     */
     public void save(Ingredient ingredient);
     
 }
